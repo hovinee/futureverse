182db92eb3d6424a6b78040291b3b2bb2b1ed387
Park KyeongKwon <team3@ducogen.net>
2024-01-11 18:24:58 +0900

    hotifx: response 일반화 장애

--- src/utils/ResponseHelper.ts
+++ src/utils/ResponseHelper.ts
@@ -2,16 +2,22 @@ import { NextResponse } from 'next/server'
 
 class ResponseHelper {
   static success(data: any = '', status: number = 200, result: number = 1000) {
-    return NextResponse.json({ result: result, ...data }, { status })
+    return NextResponse.json(
+      { result: result, resultMsg: 'success', ...data },
+      { status },
+    )
   }
 
   static error(errorMsg: string, status: number = 400, result: number = 1001) {
-    return NextResponse.json({ result: result, error: errorMsg }, { status })
+    return NextResponse.json(
+      { result: result, resultMsg: errorMsg, data: {} },
+      { status },
+    )
   }
 
   static internalError(msg: any) {
     return NextResponse.json(
-      { result: -1, error: 'Internal Server Error', message: msg },
+      { result: -1, resultMsg: 'Internal Server Error\n' + msg, data: {} },
       { status: 500 },
     )
   }
